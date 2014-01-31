ad788656aca497282e5c04c4ffa3914e953985f2
Eva Zamudio <gabriela.osu@gmail.com>
2014-01-31 15:02:53 -0800

    final updates to finish up the code

--- lib/guide.rb
+++ lib/guide.rb
@@ -1,6 +1,11 @@
 require 'restaurant'
+require 'support/string_extend'
 
 class Guide
+  class Config
+    @@actions = ['list', 'find', 'add', 'quit']
+    def self.actions; @@actions; end
+  end
 
   # having an argument will allow us to have different files potentially (ex. guide for San Francisco, a guide for Columbus, a guide for New York)
   def initialize(path=nil) 
@@ -25,24 +30,30 @@ class Guide
     result = nil
     until result == :quit
      action = get_action
-      result = do_action(action)
+     result = do_action(action)
     end
     conclusion
   end
 
   def get_action
-     print ">"
-    user_response = gets.chomp
-    action = user_response.downcase.strip
-    return action 
+     action = nil
+    # Keep asking for user input until we get a valid action
+    until Guide::Config.actions.include?(action)
+      puts "Actions: " + Guide::Config.actions.join(", ")
+      print "> "
+      user_response = gets.chomp
+      action = user_response.downcase.strip
+    end
+    return action
   end
 
-  def do_action(action)
+  def do_action(action, args=[])
     case action
     when 'list'
-      puts "Listing..."
+      list(args)
     when 'find'
-      puts 'Finding...'
+      keyword = args.shift
+      find(keyword)
     when 'add'
       add
     when 'quit'
@@ -63,6 +74,44 @@ class Guide
     end
   end
 
+  def list(args=[])
+    sort_order = args.shift
+    sort_order = args.shift if sort_order == 'by'
+    sort_order = "name" unless ['name', 'cuisine', 'price'].include?(sort_order)
+    
+    output_action_header("Listing restaurants")
+    
+    restaurants = Restaurant.saved_restaurants
+    restaurants.sort! do |r1, r2|
+      case sort_order
+      when 'name'
+        r1.name.downcase <=> r2.name.downcase
+      when 'cuisine'
+        r1.cuisine.downcase <=> r2.cuisine.downcase
+      when 'price'
+        r1.price.to_i <=> r2.price.to_i
+      end
+    end
+    output_restaurant_table(restaurants)
+    puts "Sort using: 'list cuisine' or 'list by cuisine'\n\n"
+  end
+
+   def find(keyword="")
+    output_action_header("Find a restaurant")
+    if keyword
+      restaurants = Restaurant.saved_restaurants
+      found = restaurants.select do |rest|
+        rest.name.downcase.include?(keyword.downcase) || 
+        rest.cuisine.downcase.include?(keyword.downcase) || 
+        rest.price.to_i <= keyword.to_i
+      end
+      output_restaurant_table(found)
+    else
+      puts "Find using a key phrase to search the restaurant list."
+      puts "Examples: 'find tamale', 'find Mexican', 'find mex'\n\n"
+    end
+  end
+
   def introduction
     puts "\n\n <<< Welcome to food finder >>> \n\n\n"
     puts "This is an interactive guide to help you find the food you like. \n\n"
@@ -71,6 +120,27 @@ class Guide
 
   def conclusion
     puts "\n <<< Goodbye and Buen Provecho! >>> \n\n\n"
+  end
+
+  private
+  
+  def output_action_header(text)
+    puts "\n#{text.upcase.center(60)}\n\n"
+  end
+  
+  def output_restaurant_table(restaurants=[])
+    print " " + "Name".ljust(30)
+    print " " + "Cuisine".ljust(20)
+    print " " + "Price".rjust(6) + "\n"
+    puts "-" * 60
+    restaurants.each do |rest|
+      line =  " " << rest.name.titleize.ljust(30)
+      line << " " + rest.cuisine.titleize.ljust(20)
+      line << " " + rest.formatted_price.rjust(6)
+      puts line
+    end
+    puts "No listings found" if restaurants.empty?
+    puts "-" * 60
   end
 
 
--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,9 +1,11 @@
+require 'support/number_helper'
 # Now we could just put that functionality on the guide.rb in initialize method. But that's not the best option
 # because that will not be object-oriented . Instead, it's going to be much better to have a restaurant class and 
 # put all of the functionality of dealing with that data file in the restaurant class.
 
 class Restaurant
   # a class variable here for the file path, so we dont need an instance to have the file path
+  include NumberHelper
   @@filepath = nil
 
   def self.filepath=(path=nil)
@@ -41,10 +43,18 @@ class Restaurant
   def self.saved_restaurants
     #read the restaurant file
     # return instances of restaurant
+    restaurants = []
+    if file_usable?
+      file = File.new(@@filepath, 'r')
+      file.each_line do |line|
+        restaurants << Restaurant.new.import_line(line.chomp)
+      end
+      file.close
+    end
+    return restaurants
   end
 
   def self.build_using_questions
-    
     args = {}
     print "Restaurant name: "
     args[:name] = gets.chomp.strip
@@ -54,7 +64,6 @@ class Restaurant
 
     print "Average Price: "
     args[:price] = gets.chomp.strip 
-
     return self.new(args)
   end
 
@@ -64,12 +73,22 @@ class Restaurant
     @price = args[:price]     || ""
   end
 
+  def import_line(line)
+    line_array = line.split("\t")
+    @name, @cuisine, @price = line_array
+    return self
+  end
+
   def save
     return false unless Restaurant.file_usable?
     File.open(@@filepath, 'a') do |file|
       file.puts "#{[@name, @cuisine, @price].join("\t")}\n"
     end
     return true
+  end
+
+   def formatted_price
+    number_to_currency(@price)
   end
 
 end
