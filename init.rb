043f3096b392327ec92b547ef32a891c3bccc87e
Eva Zamudio <gabriela.osu@gmail.com>
2014-01-30 18:10:10 -0800

    finish configuration in the init file

--- init.rb
+++ init.rb
@@ -5,8 +5,20 @@
 # FOOD FINDER
 # Launch this Ruby file from the command line to get started
 
-APP_ROOT = File.dirname(__FILE__)
+  APP_ROOT = File.dirname(__FILE__)
 
 
 # Define our application path
-require "#{APP_ROOT}/lib/guide"
+  # require "#{APP_ROOT}/lib/guide"
+
+# Or you can use the following code:
+# This code replace the above code and this could be a little bit better to use because it will use the separators based on the operating system. 
+# And we can use guide.rb or we can just do the same thing 
+# and leave the rb off, and it will still know where we are:
+
+  # require File.join(APP_ROOT, 'lib', 'guide.rb')
+  # require File.join(APP_ROOT, 'lib', 'guide')
+
+# More advance technique:
+$: .unshift(File.join(APP_ROOT, 'lib'))
+require 'guide'
